b46681f01a0d84537a8ba9b69254cff7b26c611e
deepakkasera <deepak.kasera143.dk@gmail.com>
2016-11-06 01:21:24 +0530

    Codeforces

---
+++ 599A.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	int d1,d2,d3;
+	cin>>d1>>d2>>d3;
+	cout<<min(min(2*d1+2*d2,d1+d3+d2),min(2*d1+2*d3,2*d2+2*d3));
+	return 0;
+}
